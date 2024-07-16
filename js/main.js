a21d912db6a4f97ed6b3e8516ccf4c1ed9aa061c
BerkantKazangirler <berkantkazangirler@gmail.com>
2024-07-16 13:39:15 +0300

    feat: last search storage, responsive fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const search = document.querySelector("#search");
 const btn = document.querySelector("#btn");
 const html = document.querySelector("html");
-var themevalue = 0
+var themevalue = 0;
+let lastsearch = "";
 const themebtn = document.querySelector("#theme");
 
 const namee = document.querySelector("#name")
@@ -17,38 +18,69 @@ const city = document.querySelector("#city")
 const blogsite = document.querySelector("#blogsite")
 const duty = document.querySelector("#duty")
 
+search.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    btn.click();
+  }
+});
+
 btn.addEventListener("click", (e) => {
   let userName = search.value.split(" ").join("");
 
   fetch("https://api.github.com/users/" + userName)
   .then(responve=>responve.json())
   .then(data => {
-    if(data.company == null) {
-      duty.innerHTML = "Not available"
+    if(data.status == "404") {
+      alert(userName+" Bulunamadı")
     } else {
-      duty.innerHTML = data.company
+      if(data.company == null) {
+        duty.innerHTML = "Not available"
+      } else {
+        duty.innerHTML = data.company
+      }
+      if(data.location == null) {
+        city.innerHTML = "Not available"
+      } else {
+        city.innerHTML = data.location
+      }
+      if(data.twitter_username == null) {
+        twitter.innerHTML = "Not available"
+      } else {
+        twitter.innerHTML = data.twitter_username
+      }
+      console.log(data)
+      namee.innerHTML = data.name
+      img.src = data.avatar_url
+      lastsearch = data.login;
+      url.innerHTML = "@"+data.login
+      url.href = data.html_url
+  
+      let joinFullDate = data.created_at;
+      let dateStr = joinFullDate.substring(0, 10);
+      let dateSplit = dateStr.split("-");
+      const monthNumber = dateSplit[1] - 1;
+      const monthName = new Date(Date.UTC(0, monthNumber)).toLocaleString(
+        "en-US",
+        { month: "short" },
+      );
+  
+      joindate.innerHTML = "Joined "+dateSplit[2]+" "+monthName+" "+dateSplit[0];
+      if(data.bio == null) {
+        bio.innerHTML = "This profile has no bio"
+      } else {
+        bio.innerHTML = data.bio
+      }
+      followers.innerHTML = data.followers
+      reponum.innerHTML = data.public_repos
+      following.innerHTML = data.following
+      if(data.blog == null) {
+        blog = "No Site"
+      } else {
+        blogsite.innerText = data.blog
+      }
+      storageLst();
     }
-    if(data.location == null) {
-      city.innerHTML = "Not available"
-    } else {
-      city.innerHTML = data.location
-    }
-    if(data.twitter_username == null) {
-      twitter.innerHTML = "Not available"
-    } else {
-      twitter.innerHTML = data.twitter_username
-    }
-    console.log(data)
-    namee.innerHTML = data.name
-    img.src = data.avatar_url
-    url.innerHTML = data.login
-    url.href = data.html_url
-    joindate.innerHTML = data.created_at
-    bio.innerHTML = data.bio
-    followers.innerHTML = data.followers
-    reponum.innerHTML = data.public_repos
-    following.innerHTML = data.following
-    blogsite.innerText = data.blog
   })
 })
 
@@ -65,6 +97,15 @@ themebtn.addEventListener("click", (e) => {
   }
 })
 
+const loadlast = () => {
+  if (lastsearch == "") {
+    console.log("Yeni Giriş")
+  } else {
+    document.querySelector("#search").value = lastsearch;
+    //btn.click();
+  }
+}
+
 const loadtheme = () => {
   if(themevalue == 0) {
     html.classList.remove("dark");
@@ -75,15 +116,23 @@ const loadtheme = () => {
   }
 }
 
+const storageLst = () => {
+  window.localStorage.setItem("last-search", JSON.stringify(lastsearch))
+}
+
 const setStorage = () => {
   window.localStorage.setItem("theme-value", JSON.stringify(themevalue))
 }
 
 const getStore = () => {
+  const e = JSON.parse(window.localStorage.getItem("last-search"))
+  lastsearch = e === null ? [] : e
+
   const d = JSON.parse(window.localStorage.getItem("theme-value"))
-  console.log("Log " + d)
   themevalue = d === null ? [] : d
+
   loadtheme();
+  loadlast();
 }
 
 window.addEventListener('load', () => {
